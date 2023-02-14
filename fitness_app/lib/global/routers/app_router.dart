f2665eed13ad354d22f362dd511367b6f00634f4
“NhiLuu” <nhi.luu@team.enouvo.com>
2023-02-14 22:49:17 +0700

    feat/sign_up_ui: add sign up ui

--- fitness_app/lib/global/routers/app_router.dart
+++ fitness_app/lib/global/routers/app_router.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:fitness_app/modules/login/login_page.dart';
+import 'package:fitness_app/modules/signup/sign_up_page.dart';
 import 'package:fitness_app/modules/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
@@ -15,6 +16,7 @@ part 'app_router.gr.dart';
     AutoRoute(page: OnBoardPage),
     AutoRoute(page: MainPage),
     AutoRoute(page: LoginPage),
+    AutoRoute(page: SignUpPage),
   ],
 )
 class AppRouter extends _$AppRouter {}
