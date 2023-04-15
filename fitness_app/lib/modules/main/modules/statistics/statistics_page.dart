f0388568bda529914987ba591d5f3a7c0655477c
“NhiLuu” <nhi.luu@team.enouvo.com>
2023-04-15 10:24:39 +0700

    feat/ai_chat: enhance statistics filter

--- fitness_app/lib/modules/main/modules/statistics/statistics_page.dart
+++ fitness_app/lib/modules/main/modules/statistics/statistics_page.dart
@@ -1,6 +1,9 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:fitness_app/global/enums/schedule_filter.dart';
 import 'package:fitness_app/global/gen/i18n.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:syncfusion_flutter_datepicker/datepicker.dart';
 
 import '../../../../global/themes/app_colors.dart';
 import 'widgets/statistics_body_data.dart';
@@ -15,11 +18,12 @@ class StatisticsPage extends StatefulWidget {
 }
 
 class _StatisticsPageState extends State<StatisticsPage> {
-  ScheduleFilter selectedSchedule = ScheduleFilter.daily;
+  ScheduleFilter selectedSchedule = ScheduleFilter.weekly;
 
   @override
   Widget build(BuildContext context) {
     final i18n = I18n.of(context)!;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(i18n.main_Statistics),
@@ -30,12 +34,27 @@ class _StatisticsPageState extends State<StatisticsPage> {
           const SizedBox(height: 8),
           Row(
             children: [
-              _filterItem(schedule: ScheduleFilter.daily),
               _filterItem(schedule: ScheduleFilter.weekly),
               _filterItem(schedule: ScheduleFilter.monthly),
               _filterItem(schedule: ScheduleFilter.yearly),
             ],
           ),
+          if (selectedSchedule == ScheduleFilter.monthly)
+            Padding(
+              padding: const EdgeInsets.only(top: 16),
+              child: FormBuilderField(
+                name: 'month',
+                decoration: const InputDecoration(
+                  suffixIcon: Icon(
+                    Icons.arrow_drop_down_sharp,
+                    size: 30,
+                  ),
+                ),
+                builder: (field) {
+                  return const MonthPickerDialog();
+                },
+              ),
+            ),
           const SizedBox(height: 32),
           const StatisticsBodyData(),
           const SizedBox(height: 32),
@@ -71,9 +90,9 @@ class _StatisticsPageState extends State<StatisticsPage> {
           const SizedBox(height: 16),
           const StatisticsChart(),
           const SizedBox(height: 32),
-          const Text(
-            'Recently Workout',
-            style: TextStyle(
+          Text(
+            i18n.statistics_RecentWorkout,
+            style: const TextStyle(
               fontSize: 16,
               fontWeight: FontWeight.w600,
             ),
@@ -109,6 +128,90 @@ class _StatisticsPageState extends State<StatisticsPage> {
             ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class MonthPickerDialog extends StatelessWidget {
+  const MonthPickerDialog({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final i18n = I18n.of(context)!;
+
+    void showDialogPicker() {
+      showDialog(
+          context: context,
+          builder: (context) {
+            return Dialog(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)),
+              child: Container(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    SfDateRangePicker(
+                      view: DateRangePickerView.year,
+                      initialSelectedRange: PickerDateRange(
+                        DateTime.now(),
+                        DateTime.now(),
+                      ),
+                      selectionColor: AppColors.primaryBold,
+                      todayHighlightColor: AppColors.primaryBold,
+                      allowViewNavigation: false,
+                      // minDate: widget.minDate,
+                      // maxDate: widget.maxDate,
+                      onSelectionChanged: (selectedDate) {
+                        if (selectedDate.value is PickerDateRange) {
+                          final dateRange =
+                              selectedDate.value as PickerDateRange;
+                          // setState(
+                          //   () {
+                          //     startDate =
+                          //         dateRange.startDate?.combineTime(startDate) ??
+                          //             startDate;
+                          //     endDate = dateRange.endDate?.combineTime(endDate) ??
+                          //         startDate.combineTime(endDate);
+                          //   },
+                          // );
+                        }
+                      },
+                      // selectableDayPredicate: (DateTime val) =>
+                      //     !widget.excludeWeekDay.contains(val.weekday),
+                    ),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: OutlinedButton(
+                            onPressed: context.popRoute,
+                            child: Text(i18n.button_Cancel),
+                          ),
+                        ),
+                        const SizedBox(width: 8),
+                        Expanded(
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            child: Text(i18n.button_Done),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            );
+          });
+    }
+
+    return InputDecorator(
+      decoration: const InputDecoration(),
+      child: InkWell(
+        child: Text(i18n.statistics_SelectMonth),
+        onTap: () => showDialogPicker(),
       ),
     );
   }
